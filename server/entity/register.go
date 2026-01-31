1b819e6915ef4b68a3f6a09bae1bf925d7496aaa
Niceninjapro <niceninjapro@gmail.com>
2026-01-31 18:45:15 +0000

    Remove WindChargeType from entity registry
    Removed WindChargeType and its associated instantiation from the entity registry.

--- server/entity/register.go
+++ server/entity/register.go
@@ -25,7 +25,6 @@ var DefaultRegistry = conf.New([]world.EntityType{
 	SplashPotionType,
 	TNTType,
 	TextType,
-	WindChargeType,
 })
 
 var conf = world.EntityRegistryConfig{
@@ -36,7 +35,6 @@ var conf = world.EntityRegistryConfig{
 	EnderPearl:         NewEnderPearl,
 	FallingBlock:       NewFallingBlock,
 	Lightning:          NewLightning,
-	WindCharge:         NewWindCharge,
 	Firework: func(opts world.EntitySpawnOpts, firework world.Item, owner world.Entity, sidewaysVelocityMultiplier, upwardsAcceleration float64, attached bool) *world.EntityHandle {
 		return newFirework(opts, firework.(item.Firework), owner, sidewaysVelocityMultiplier, upwardsAcceleration, attached)
 	},
